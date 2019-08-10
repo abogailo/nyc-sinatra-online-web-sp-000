a66957312570915a16ad31f26e1700f9773c837b
Abagail Hudson <abagailanne@gmail.com>
2019-08-10 17:51:52 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,33 +1,35 @@
 class LandmarksController < ApplicationController
   # add controller methods
-  get '/landmarks' do
-    @landmarks = Landmark.all
-    erb :'/landmarks/index'
-  end
-
-   get '/landmarks/new' do
-    erb :'/landmarks/new'
+  get '/landmarks/new' do
+    erb :'landmarks/new'
   end
 
    post '/landmarks' do
-    @landmark = Landmark.new(params[:landmark])
-    @landmark.save
-    redirect to "landmarks/#{@landmark.id}"
+    landmark = Landmark.create(params[:landmark])
+    redirect "landmarks/#{landmark.id}"
   end
 
-   get '/landmarks/:id/edit' do
-    @landmark = Landmark.find(params[:id])
-    erb :'/landmarks/edit'
+   #-----Read------
+
+   get '/landmarks' do
+    erb :'landmarks/index'
   end
 
    get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
-    erb :'/landmarks/show'
+    erb :'landmarks/show'
   end
 
-   post '/landmarks/:id' do
-    landmark=Landmark.find(params[:id])
+   #-----Update-----
+
+   get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+   patch '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
     landmark.update(params[:landmark])
-    redirect to "landmarks/#{landmark.id}"
+    redirect "landmarks/#{landmark.id}"
   end
 end
