484c3c90f67c0281c3c5253f411bf48425f4a1b7
Abagail Hudson <abagailanne@gmail.com>
2019-08-10 18:50:13 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,7 @@ class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
     @figures = Figure.all
-    erb :'figures/index'
+    erb :'/figures/index'
   end
 
    get '/figures/new' do
@@ -17,29 +17,39 @@ class FiguresController < ApplicationController
   end
 
    post '/figures' do
-    @figure = Figure.create(params[:figure])
-
-     if !params["title"]["name"].empty?
-      @figure.titles << Title.create(params[:title])
+    @figure = Figure.create(params["figure"])
+    if !params["title"].empty?
+      @figure.titles << Title.create(params["title"])
     end
-    if !params["landmark"]["name"].empty?
-      @figure.landmarks << Landmark.create(params[:landmark])
+    if !params["landmark"].empty?
+      @figure.landmarks << Landmark.create(params["landmard"])
     end
-
-     redirect "/figures/#{@figure.id}"
+    erb :'/figures/show'
   end
 
-   get "/figures/:id/edit" do
+   get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
     @landmarks = Landmark.all
     @titles = Title.all
-    @figure = Figure.find_by_id(params[:id])
     erb :'/figures/edit'
   end
 
    patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
-    @figure.update(params[:figure_name])
-
-     redirect "/figures/#{@figures.id}"
+    @figure.name = params["figure"]["name"]
+    @figure.save
+    if !params["title"].empty?
+      @figure.titles << Title.create(params["title"])
+    else
+      @figure.title_ids = params["figure"]["title_ids"]
+      @figure.save
+    end
+    if !params["landmark"].empty?
+      @figure.landmarks << Landmark.create(params["landmark"])
+    else
+      @figure.landmark_ids = params["figure"]["landmark_ids"]
+      @figure.save
+    end
+    redirect to("figures/#{params[:id]}")
   end
 end
