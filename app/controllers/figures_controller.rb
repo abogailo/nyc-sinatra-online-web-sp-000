5500eb229175419d37de6008e6b075f177850cb6
Abagail Hudson <abagailanne@gmail.com>
2019-08-10 18:22:29 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,51 +2,96 @@ class FiguresController < ApplicationController
   # add controller methods
   get '/figures' do
     @figures = Figure.all
-    erb :'/figures/index'
+
+     erb :"/figures/index"
   end
 
-  get '/figures/new' do
-    erb :'/figures/new'
+   get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+     erb :"/figures/new"
   end
 
-  post '/figures' do
-    @figure = Figure.new(params[:figure])
-
-     if !params[:title][:name].empty?
-      @figure.titles.build(name: params[:title][:name])
-  end
-
-  if !params[:landmark][:name].empty?
-    @figure.landmarks.build(name: params[:landmark][:name])
-  end
-
-    @figure.save
-    redirect to "figures/#{@figure.id}"
-  end
-
-  get '/figures/:id/edit' do
+   get '/figures/:id' do
     @figure = Figure.find(params[:id])
-    erb :'/figures/edit'
-  end
+    @landmarks = @figure.landmarks
+    @titles = @figure.titles
 
-  get '/figures/:id' do
-    @figure = Figure.find(params[:id])
-    erb :'/figures/show'
-  end
+     erb :"/figures/show"
+  end 
 
-  post '/figures/:id' do
-    @figure = Figure.find(params[:id])
-    @figure.update(params[:figure])
+   post '/figures' do
+    @figure = Figure.find_or_create_by(name: params[:figure][:name])
 
-     if !params[:title][:name].empty?
-      @figure.titles.build(name: params[:title][:name])
+     title_ids = params[:figure][:title_ids]
+    if title_ids != nil && !title_ids.empty?    
+      title_ids.each do |title_id|
+        @figure.titles << Title.find(title_id)
+      end
     end
 
-  if !params[:landmark][:name].empty?
-      @figure.landmarks.build(name: params[:landmark][:name])
+     if params[:title] != nil && params[:title][:name].size > 1
+      @figure.titles << Title.create(name: params[:title][:name])
     end
 
-  @figure.save
-    redirect to "figures/#{@figure.id}"
+     landmark_ids = params[:figure][:landmark_ids]
+    if landmark_ids!= nil && !landmark_ids.empty?       
+      landmark_ids.each do |landmark_id|
+        @figure.landmarks << Landmark.find(landmark_id)
+      end
+    end
+
+     if params[:landmark] != nil && params[:landmark][:name].size > 1
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+      @figure.save
+    end
+
+     @figure.save
+
+     redirect "/figures/#{@figure.id}"
   end
-end
+
+   get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+     erb :"/figures/edit"
+  end
+
+   patch "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    @figure.landmarks.clear
+    @figure.titles.clear
+
+     @figure.update(name: params[:figure][:name])
+
+     title_ids = params[:figure][:title_ids]
+    if title_ids != nil && !title_ids.empty?    
+      title_ids.each do |title_id|
+        @figure.titles << Title.find(title_id)
+      end
+    end
+
+     if params[:title] != nil && params[:title][:name].size > 1
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+
+     landmark_ids = params[:figure][:landmark_ids]
+    if landmark_ids!= nil && !landmark_ids.empty?       
+      landmark_ids.each do |landmark_id|
+        @figure.landmarks << Landmark.find(landmark_id)
+      end
+    end
+
+     if params[:landmark] != nil && params[:landmark][:name].size > 1
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+      @figure.save
+    end
+
+     @figure.save
+
+     redirect "/figures/#{@figure.id}"
+  end
+end	
